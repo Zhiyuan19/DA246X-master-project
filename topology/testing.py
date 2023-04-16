914527d386942414a6d8897579b25a3264e252a6
mezbahul <mezbahul@kth.se>
2023-04-16 16:27:24 +0200

    updated ping-> sleep

--- topology/testing.py
+++ topology/testing.py
@@ -1,14 +1,16 @@
 import topology
+from time import sleep
 
 
-def ping(client, server, expected, count=1, wait=4):
+def ping(client, server, expected, count=1, wait=1):
 
-    ping_result = client.cmd(f'ping {server.IP()} -c 1 -w 4')
-    # print(ping_result)
     # ping_result = client.cmd(f'ping {server.IP()} -c 1 -w 4')
+
     # print(ping_result)
 
     cmd = f"ping {server.IP()} -c {count} -W {wait} >/dev/null 2>&1; echo $?"
+
+    sleep(1)
 
     ret = client.cmd(cmd)
 
@@ -26,10 +28,12 @@ def ping(client, server, expected, count=1, wait=4):
 
     print("ret value:", ret)
 
-    if ret == 0:
-        return expected
+    if int(ret) == 0:
+
+        print("printing...")
 
     else:
+
         return False
 
 
@@ -59,16 +63,15 @@ def ping(client, server, expected, count=1, wait=3):
 
 
 def curl(client, server, method="POST", payload="Group2", port=80, expected=True):
-
     """
     run curl for HTTP request. Request method and payload should be specified
     Server can either be a host or a string
     return True in case of success, False if not
     """
 
-    #print(client.name, client.IP())
+    # print(client.name, client.IP())
 
-    #print(server.name, server.IP())
+    # print(server.name, server.IP())
 
     if (isinstance(server, str) == 0):
         server_ip = str(server.IP())
@@ -79,24 +82,24 @@ def curl(client, server, method="POST", payload="Group2", port=80, expected=True
     # TODO: Pass some payload (a.k.a. data). You may have to add some escaped quotes!
     # The magic string at the end reditect everything to the black hole and just print the return code
 
-    #print(server_ip)
+    # print(server_ip)
 
-    #print(server)
+    # print(server)
 
-    #print(client)
+    # print(client)
     cmd = f"curl --connect-timeout 3 --max-time 3 -X {method} -d '{payload}' -v -s {server_ip}:{port}/cgi-bin/index.cgi > /dev/null 2>&1; echo $? "
-  
+
     ret = client.cmd(cmd).strip()
 
     print(f"`{cmd}` on {client} returned {ret}")
     # TODO: What value do you expect?
-  
+
     if ret == "0":
 
         print("ret value:", ret)
 
         return True
-    #else:
+    # else:
 
-        #return False
-    #return True  # True means "everyhing went as expected"
+        # return False
+    # return True  # True means "everyhing went as expected"
